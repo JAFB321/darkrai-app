5ffbdfc9f2d1b408828b8472ff0c84850e5da16e
JAFB321 <jafb321@gmail.com>
2023-05-02 18:41:48 -0700

    chore: add server Api to next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const nodeIP = require('ip')
+
 module.exports = {
   experimental: {
     newNextLinkBehavior: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  env: {
+    SERVER_API_IP: nodeIP.address(),
   },
 };
